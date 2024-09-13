18bf16c2cb92e5af16d01ad8ad03dba2f795e91c
Sam Millay <sammillay@MacBook-Pro-2.local>
2024-09-13 12:10:37 +0200

    requirements 4

--- SeasFuture.py
+++ SeasFuture.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, abort, url_for
+from flask import Flask, render_template, request, jsonify, url_for
 from flask_cors import CORS
 import os
 import requests
